d7e1348084539d7f04be19e53f2d38428220e8a7
Serge Chvorun <elifnull@protonmail.com>
2022-01-27 12:59:31 -0500

    Validation loop update, controller simplified

--- flask_app/controllers/emails.py
+++ flask_app/controllers/emails.py
@@ -1,7 +1,19 @@
+from crypt import methods
 from flask_app import app
-from flask import flash, render_template
+from flask_app.models.email import Email
+from flask import flash, render_template, request, redirect
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/create_email", methods=["POST"])
+def email_create():
+    if not Email.validate_email(request.form):
+        return redirect("/")
+    Email.save(request.form)
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("results.html", emails = Email.get_all())
